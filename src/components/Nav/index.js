3a869d0b99896ae9ea609b7036d811b197cf410e
Carmen Guevara <carmen.guevara75@gmail.com>
2022-02-23 16:47:23 -0500

    ReactFrond Project

---
+++ src/components/Nav/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Nav = () => {
+    return (
+        <div className="ui secondary pointing menu">
+        {/* <Link className="item active">
+          Home
+        </Link> */}
+        <Link className="item" to ='students'>
+          Students
+        </Link>
+        {/* <Link className="item">
+          Classes
+        </Link> */}
+        {/* <div className="right menu">
+          <Link className="ui item">
+            Login
+          </Link>
+        </div> */}
+      </div>
+    );
+}
+
+export default Nav;
